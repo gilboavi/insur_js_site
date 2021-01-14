37b2e3a8b32ae7e5ea9d027d9177efd4fbaf06a9
Boaz Foux <w7.bfgun@gmail.com>
2021-01-14 06:16:23 +0200

    add db tables

---
+++ db/agents.sql
@@ -0,0 +1,10 @@
+USE `aluma_db`;
+
+
+CREATE TABLE agents(
+	`serial` int AUTO_INCREMENT NOT NULL,
+	`agent_name` nvarchar(50) NULL,
+ PRIMARY KEY (	`serial` ASC) 
+) 
+
+
---
+++ db/param_work_status.sql
@@ -0,0 +1,9 @@
+
+
+
+CREATE TABLE param_work_status (
+    `serial` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `param_name` NVARCHAR(50) NULL
+)
+
+
